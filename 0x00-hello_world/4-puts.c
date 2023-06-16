5628a73143fe4f1ace6ced70ebc0525e1b1c011a
Glorg <eglory094@gmail.com>
2023-06-16 07:13:29 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**                                                    * main - typrint a string with c                      * Decsription: printing programming is like a building a multiple puzzle                                    * Return: 0
+ */ 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+}   
